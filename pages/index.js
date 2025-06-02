fe0079dd2785158308ac82489c5621106d671dcb
Chronic-W-F <vandergriff4004@comcast.net>
2025-06-02 02:57:39 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
+import Navbar from '../components/Navbar';
 import SeedEntryForm from '../components/SeedEntryForm';
 
 export default function Home() {
   return (
-    <div className="p-6 max-w-4xl mx-auto">
-      <h1 className="text-2xl font-bold mb-4">🌱 Public Seed Vault</h1>
-      <SeedEntryForm />
-    </div>
+    <>
+      <Navbar />
+      <div className="p-6 max-w-4xl mx-auto">
+        <h1 className="text-2xl font-bold mb-4">🌱 Public Seed Vault</h1>
+        <SeedEntryForm />
+      </div>
+    </>
   );
 }
