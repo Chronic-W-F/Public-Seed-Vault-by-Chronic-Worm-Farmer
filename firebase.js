aecd60028f2e587bc49493609c892e5f6dea42b7
Chronic-W-F <vandergriff4004@comcast.net>
2025-06-02 01:39:16 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,16 +3,14 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDtAKBVp6Hyf2eXv1Wo0YLuuE1hfZ80MgM",
+  authDomain: "public-seed-vault-app.firebaseapp.com",
+  projectId: "public-seed-vault-app",
+  storageBucket: "public-seed-vault-app.firebasestorage.app",
+  messagingSenderId: "194363722248",
+  appId: "1:194363722248:web:7ca20411d166c164089fbd",
+  measurementId: "G-6DEP8JY9VP"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
-
+export const db = getFirestore(app);
