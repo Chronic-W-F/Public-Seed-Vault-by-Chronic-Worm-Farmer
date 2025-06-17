fc8085fe25024997a00681b6ba7dc55863e8b966
Chronic-W-F <vandergriff4004@comcast.net>
2025-06-17 03:52:18 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 import BackgroundLayout from '../components/BackgroundLayout';
 
 export default function App({ Component, pageProps }) {
   return (
-    <BackgroundLayout>
-      <Component {...pageProps} />
-    </BackgroundLayout>
+    <>
+      <Head>
+        <title>Chronic Seed Vault</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <BackgroundLayout>
+        <Component {...pageProps} />
+      </BackgroundLayout>
+    </>
   );
 }
