b1d725dac680ccf3afd511c836fd6edb534ce3a5
Chronic-W-F <vandergriff4004@comcast.net>
2025-06-17 03:08:59 -0500

    Update BackgroundLayout.js

--- components/BackgroundLayout.js
+++ components/BackgroundLayout.js
@@ -2,7 +2,7 @@ export default function BackgroundLayout({ children }) {
   return (
     <div
       className="min-h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center"
-      style={{ backgroundImage: "url('/vault-bg.jpg')" }} // Change if you're using .png
+      style={{ backgroundImage: "url('/vault-bg.png')" }} // Change if you're using .png
     >
       <div className="min-h-screen w-full bg-white/80 backdrop-blur-sm">
         {children}
